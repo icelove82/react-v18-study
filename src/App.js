9462378f13abfa03f96bc622aa09a23551fad374
YUN <yunmyeonghun@gmail.com>
2024-01-19 15:47:33 +0900

    slot sample

--- src/App.js
+++ src/App.js
@@ -31,12 +31,40 @@ export function Profile(props) {
   );
 }
 
+export function Card({ children }) {
+  return (
+    <div className="card">
+      <div className="card-content">{children}</div>
+    </div>
+  );
+}
+
 export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
       <Profile {...profiles[0]} size={90} />
       <Profile {...profiles[1]} size={90} />
+
+      <br />
+
+      <Card>
+        <h1>Photo</h1>
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card>
+        <h1>About</h1>
+        <p>
+          Aklilu Lemma was a distinguished Ethiopian scientist who discovered a
+          natural treatment to schistosomiasis.
+        </p>
+      </Card>
     </div>
   );
 }
