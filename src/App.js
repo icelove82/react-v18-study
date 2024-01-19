c926153dde9f7c1f0a2975301f199490281b93cd
YUN <yunmyeonghun@gmail.com>
2024-01-19 14:26:01 +0900

    my fist component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import { getImageUrl } from "./util.js";
+
+const profiles = [
+  {
+    name: "Maria Skłodowska-Curie",
+    imgId: "szV5sdG",
+    pro: "physicist and chemist",
+    awd: [
+      "Nobel Prize in Physics",
+      "Nobel Prize in Chemistry",
+      "Davy Medal",
+      "Matteucci Medal",
+    ],
+    dsv: "polonium (chemical element)",
+  },
+  {
+    name: "Katsuko Saruhashi",
+    imgId: "YfeOqp2",
+    pro: "physicist and chemist",
+    awd: ["Miyake Prize for geochemistry", "Tanaka Prize"],
+    dsv: "a method for measuring carbon dioxide in seawater",
+  },
+];
+
+function getAwdStr(awd) {
+  let awdStr = "";
+
+  awd.forEach((it, idx) => {
+    if (idx === 0) {
+      awdStr += it;
+    } else {
+      awdStr += ", " + it;
+    }
+    return awdStr;
+  });
+}
+export function Profile(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="profile">
+      <h2>props.name</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(props.imgId)}
+        alt="props.name"
+        width={70}
+        height={70}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {props.pro}
+        </li>
+        <li>
+          <b>Awards: {props.awd.length} </b>({getAwdStr(props.awd)})
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {props.dsv}
+        </li>
+      </ul>
+    </section>
   );
 }
 
-export default App;
+export default function Gallery() {
+  return (
+    <div>
+      <h1>Notable Scientists</h1>
+      <Profile {...profiles[0]} />
+      <Profile {...profiles[1]} />
+    </div>
+  );
+}
